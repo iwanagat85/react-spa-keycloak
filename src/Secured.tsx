ae92f763fc6405fef95282cd5074434162c725ac
iwanagat85 <s19hwr890or7smd5@gmail.com>
2019-11-08 14:52:52 +0900

    envをdocker-compose側に

--- src/Secured.tsx
+++ src/Secured.tsx
@@ -1,6 +1,5 @@
 import React, {useEffect, useState} from 'react';
 import Alert from 'react-bootstrap/Alert'
-import Button from 'react-bootstrap/Button'
 
 import KeycloakState from "./KeycloakState";
 
